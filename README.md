2e038c7a1f293309e7011f0d572e35bd97d58c69
Akanksha Reddy <akankshareddy711@gmail.com>
2026-03-08 10:44:46 +0530

    improved ML project with visualization

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-Student Score Prediction using Machine Learning.
+# Student Score Prediction
 
-This project predicts student exam scores based on study hours using Linear Regression.
+This project uses Machine Learning to predict student exam scores based on the number of hours studied.
+
+Technologies Used:
+- Python
+- Scikit-learn
+- NumPy
+- Matplotlib
+
+The model is trained using Linear Regression.
+
+Features:
+- Train ML model on study hours dataset
+- Predict exam scores
+- Visualize results using graphs
--- student_score.py
+++ student_score.py
@@ -1,15 +1,24 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
-# dataset
-hours = np.array([1,2,3,4,5]).reshape(-1,1)
-scores = np.array([20,35,45,55,65])
+# Dataset: study hours vs scores
+hours = np.array([1,2,3,4,5,6,7,8]).reshape(-1,1)
+scores = np.array([20,30,40,50,60,70,80,90])
 
-# model
+# Train model
 model = LinearRegression()
 model.fit(hours, scores)
 
-# prediction
-pred = model.predict([[6]])
+# Predict score for 9 hours study
+prediction = model.predict([[9]])
 
-print("Predicted score for 6 hours study:", pred[0])
+print("Predicted score for 9 hours study:", prediction[0])
+
+# Visualization
+plt.scatter(hours, scores, color="blue")
+plt.plot(hours, model.predict(hours), color="red")
+plt.xlabel("Study Hours")
+plt.ylabel("Scores")
+plt.title("Student Score Prediction")
+plt.show()
